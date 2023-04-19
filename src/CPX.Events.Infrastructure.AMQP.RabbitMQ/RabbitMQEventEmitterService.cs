512a03e0bc04ba30ef10c2e458f85c5ea87cf6c8
Cassiano Peixoto <cassiano.peixoto@ardanis.com>
2023-04-19 19:54:47 +0100

    Creating the RabbitMQEventEmitterService

---
+++ src/CPX.Events.Infrastructure.AMQP.RabbitMQ/RabbitMQEventEmitterService.cs
@@ -0,0 +1,24 @@
+﻿using CPX.Events.Abstract;
+using CPX.Events.Infrastructure.AMQP.Abstract;
+
+namespace CPX.Events.Infrastructure.AMQP.RabbitMQ;
+
+public sealed class RabbitMQEventEmitterService : IEventEmitterService
+{
+    private readonly string routingKey;
+
+    private readonly IPublishService publishService;
+
+    public RabbitMQEventEmitterService(string routingKey, IPublishService? publishService)
+    {
+        if (publishService is null) throw new ArgumentNullException(nameof(publishService));
+
+        this.routingKey = routingKey;
+        this.publishService = publishService;
+    }
+
+    public void Emit(Event @event)
+    {
+        publishService.Publish(routingKey, @event);
+    }
+}
---
+++ tests/CPX.Events.Infrastructure.AMQP.RabbitMQ.Test/RabbitMQEventEmitterServiceTest.cs
@@ -0,0 +1,39 @@
+using CPX.Events.Infrastructure.AMQP.Abstract;
+using CPX.Events.Infrastructure.AMQP.RabbitMQ.Mocks;
+
+namespace CPX.Events.Infrastructure.AMQP.RabbitMQ.Test;
+
+public sealed class RabbitMQEventEmitterServiceTest
+{
+    [Fact]
+    public void Should_not_be_able_to_instantiate_with_null_publish_service()
+    {
+        // Arrange
+        var routingKey = "routingKey";
+        IPublishService? publishService = null;
+        // Act
+        // Assert
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            new RabbitMQEventEmitterService(routingKey, publishService);
+        });
+    }
+
+    [Fact]
+    public void Should_be_able_to_emit_event()
+    {
+        // Arrange
+        var createdAt = DateTimeOffset.UtcNow;
+        var @event = new FooEvent(createdAt);
+        var routingKey = "routingKey";
+        var mockPublishService = new Mock<IPublishService>();
+        mockPublishService.Setup(o => o.Publish(routingKey, @event)).Verifiable();
+        var publishService = mockPublishService.Object;
+        // Act
+        var rabbitMQEventEmitterService = new RabbitMQEventEmitterService(routingKey, publishService);
+        rabbitMQEventEmitterService.Emit(@event);
+        // Assert
+        Assert.IsAssignableFrom<IEventEmitterService>(rabbitMQEventEmitterService);
+        mockPublishService.VerifyAll();
+    }
+}
